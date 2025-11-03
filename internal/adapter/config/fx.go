4912a7a9422d41acdf5aa8bd695e9120812afc94
Simone <simonesamardzhiev@gmail.com>
2025-11-03 17:54:53 +0200

    refactor(config): Make fx config module separate the config structs

--- internal/adapter/config/fx.go
+++ internal/adapter/config/fx.go
@@ -5,4 +5,13 @@ import "go.uber.org/fx"
 var Module = fx.Module(
 	"config",
 	fx.Provide(New),
+	fx.Provide(func(container *Container) *AppConfig {
+		return &container.AppConfig
+	}),
+	fx.Provide(func(container *Container) *DBConfig {
+		return &container.DbConfig
+	}),
+	fx.Provide(func(container *Container) *AuthConfig {
+		return &container.AuthConfig
+	}),
 )
