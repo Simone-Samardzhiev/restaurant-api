e3ad2e43459ec9c24c052f94df70f74d009498aa
Simone <simonesamardzhiev@gmail.com>
2025-11-03 19:52:09 +0200

    feat: Add Taskfile.yaml for running migrations and add migration for products

---
+++ internal/adapter/storage/postgres/migrations/000001_create_dishes_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE products_categories
+(
+    id   UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL CHECK ( length(name) > 4 )
+);
+
+CREATE TABLE products
+(
+    id          UUID PRIMARY KEY,
+    name        VARCHAR(100) UNIQUE                      NULL CHECK ( length(name) > 3 ),
+    description TEXT                                     NOT NULL CHECK ( length(description) > 15 ),
+    image_url   VARCHAR(200)                             NULL CHECK ( length(description) > 20 ),
+    category    UUID REFERENCES products_categories (id) NOT NULL,
+    price       DECIMAL(8, 2)                            NOT NULL CHECK ( price > 0 )
+);
+
