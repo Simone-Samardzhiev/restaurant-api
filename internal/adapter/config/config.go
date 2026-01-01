cb1c265294466d768991b93602ccdb671441d5ec
simone <simonesamardzhiev@gmail.com>
2026-01-01 16:00:42 +0100

    refactor(config): Make use of `github.com/kelseyhightower/envconfig ` for easier loading and parsing environment variables

--- internal/adapter/config/config.go
+++ internal/adapter/config/config.go
@@ -2,35 +2,12 @@ package config
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"unicode/utf8"
+	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
 )
-
-// getEnv is a helper function for getting environment variable,
-// if the variable doesn't exist fallback is returned.
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
-// getEnvInt is a helper function for getting environment variable parsed as int,
-// if the variable doesn't exist or is not a valid int, fallback is returned.
-func getEnvInt(key string, fallback int) int {
-	if value, ok := os.LookupEnv(key); ok {
-		parsedValue, err := strconv.Atoi(value)
-		if err != nil {
-			return fallback
-		}
-		return parsedValue
-	}
-	return fallback
-}
 
 type (
 	// Environment for different app environments.
@@ -45,22 +22,21 @@ type (
 
 	// AppConfig holds all environment variable for the application.
 	AppConfig struct {
-		Environment Environment
-		Port        string
+		Environment Environment `envconfig:"ENVIRONMENT" required:"true"`
+		Port        string      `envconfig:"PORT" default:":8080"`
 	}
 
 	// StorageConfig holds all environment variable for the database.
 	StorageConfig struct {
-		DbUrl                string
-		DbMaxIdleConnections int
-		DbMaxOpenConnections int
-		ImagesApiKey         string
+		DbUrl                string `envconfig:"DB_URL" required:"true"`
+		DbMaxIdleConnections int    `envconfig:"DB_MAX_IDLE_CONNECTIONS" default:"5"`
+		DbMaxOpenConnections int    `envconfig:"DB_MAX_OPEN_CONNECTIONS" default:"5"`
 	}
 
 	// AuthConfig holds all environment variable for the authentication.
 	AuthConfig struct {
-		Username string
-		Password string
+		Username string `envconfig:"USERNAME" required:"true"`
+		Password string `envconfig:"PASSWORD" required:"true"`
 	}
 )
 
@@ -69,86 +45,28 @@ const (
 	Production  Environment = "production"
 )
 
-func newAppConfig() (AppConfig, error) {
-	port := getEnv("PORT", ":8080")
-	environment := Environment(getEnv("ENVIRONMENT", "development"))
-	if environment != Production && environment != Development {
-		return AppConfig{}, fmt.Errorf("invalid environment: %s", environment)
+func (e *Environment) Decode(value string) error {
+	switch strings.ToLower(value) {
+	case "development":
+		*e = Development
+	case "production":
+		*e = Production
+	default:
+		return fmt.Errorf("invalid environment value: %s", value)
 	}
-
-	return AppConfig{
-		Port:        port,
-		Environment: environment,
-	}, nil
-}
-
-func newStorageConfig() (StorageConfig, error) {
-	dbURL := os.Getenv("DB_URL")
-	if dbURL == "" {
-		return StorageConfig{}, fmt.Errorf("DB_URL environment variable not set")
-	}
-
-	maxIdleConnections := getEnvInt("DB_MAX_IDLE_CONNECTIONS", 10)
-	if maxIdleConnections <= 0 {
-		return StorageConfig{}, fmt.Errorf("max idle connections must be greater than zero: %d", maxIdleConnections)
-	}
-	maxOpenConnections := getEnvInt("DB_MAX_OPEN_CONNECTIONS", 10)
-	if maxOpenConnections <= 0 {
-		return StorageConfig{}, fmt.Errorf("max open connections must be greater than zero: %d", maxOpenConnections)
-	}
-
-	localFilesPath := os.Getenv("IMAGES_API_KEY")
-	if localFilesPath == "" {
-		return StorageConfig{}, fmt.Errorf("images api key environment variable not set")
-	}
-
-	return StorageConfig{
-		DbUrl:                dbURL,
-		DbMaxIdleConnections: maxIdleConnections,
-		DbMaxOpenConnections: maxOpenConnections,
-		ImagesApiKey:         localFilesPath,
-	}, nil
-}
-
-func newAuthConfig() (AuthConfig, error) {
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
-
-	if utf8.RuneCountInString(username) < 8 {
-		return AuthConfig{}, fmt.Errorf("username must be at least 8 characters")
-	}
-	if utf8.RuneCountInString(password) < 8 {
-		return AuthConfig{}, fmt.Errorf("password must be at least 8 characters")
-	}
-	return AuthConfig{
-		Username: username,
-		Password: password,
-	}, nil
+	return nil
 }
 
 func New() (*Container, error) {
 	if err := godotenv.Load(); err != nil {
-		log.Println("Error loading .env file")
+		zap.L().Warn("error loading .env file", zap.Error(err))
 	}
 
-	appConfig, err := newAppConfig()
-	if err != nil {
-		return nil, err
+	var container Container
+
+	if err := envconfig.Process("", &container); err != nil {
+		return nil, fmt.Errorf("error mapping environemnt variables: %w", err)
 	}
 
-	storageConfig, err := newStorageConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	authConfig, err := newAuthConfig()
-	if err != nil {
-		return nil, err
-	}
-
-	return &Container{
-		AppConfig:  appConfig,
-		DbConfig:   storageConfig,
-		AuthConfig: authConfig,
-	}, nil
+	return &container, nil
 }
