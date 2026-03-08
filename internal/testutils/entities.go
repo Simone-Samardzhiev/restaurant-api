2600782aef0a34e4c0cfd564616b01a65baea361
simone <simonesamardzhiev@gmail.com>
2026-03-08 12:29:07 +0200

    test(entities): Add better messaging to helper function for checking identifiable entities

--- internal/testutils/entities.go
+++ internal/testutils/entities.go
@@ -3,22 +3,22 @@ package testutils
 import "testing"
 
 // CheckEntities is a test helper that check entities that are identifiable by id.
-// It checks if all ids match the ids of the entities by calling getId.
-func CheckEntities[T any, K comparable](t testing.TB, expectedIds []K, entities []T, getId func(T) K) {
+// It checks if the ids match the ids of the entities by calling getId.
+func CheckEntities[T any, K comparable](t testing.TB, wantIds []K, entities []T, getId func(T) K) {
 	t.Helper()
 
-	if len(expectedIds) != len(entities) {
-		t.Errorf("want %d entities, got %d", len(expectedIds), len(entities))
+	if len(wantIds) != len(entities) {
+		t.Errorf("want %d ids, got %d", len(wantIds), len(entities))
 	}
 
-	counter := make(map[K]int, len(expectedIds))
-	for _, id := range expectedIds {
+	counter := make(map[K]int, len(wantIds))
+	for _, id := range wantIds {
 		counter[id]++
 	}
 
 	for _, entity := range entities {
 		if counter[getId(entity)] == 0 {
-			t.Errorf("missing entity with id: %v", getId(entity))
+			t.Errorf("unexpected : %v", getId(entity))
 			continue
 		}
 		counter[getId(entity)]--
