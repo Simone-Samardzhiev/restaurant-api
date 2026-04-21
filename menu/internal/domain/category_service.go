32e84c8fcd26ee09565261b8eb56601ff6034944
simone <simonesamardzhiev@gmail.com>
2026-04-21 22:02:24 +0300

    chore(menu): Add default implementation of `CategoryService`

---
+++ menu/internal/domain/category_service.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// DefaultCategoryService is the default implementation of [CategoryService].
+type DefaultCategoryService struct {
+	repository CategoryRepository
+}
+
+var _ CategoryService = (*DefaultCategoryService)(nil)
+
+// NewDefaultCategoryService creates and allocates new [DefaultCategoryService].
+func NewDefaultCategoryService(repository CategoryRepository) *DefaultCategoryService {
+	return &DefaultCategoryService{repository: repository}
+}
+
+func (d *DefaultCategoryService) Add(ctx context.Context, name string) (*Category, error) {
+	now := time.Now()
+
+	category := Category{
+		Id:        uuid.New(),
+		Name:      name,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	if err := d.repository.Save(ctx, &category); err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
