4b25bf1ef54a1f99f497a9050b8d22d32088832d
simone <simonesamardzhiev@gmail.com>
2026-05-16 14:18:25 +0300

    feat(menu/storage): Add `ImageStorage` implementation using S3 bucket

--- menu/go.mod
+++ menu/go.mod
@@ -3,6 +3,8 @@ module menu
 go 1.26.0
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.41.7
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.101.0
 	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
@@ -13,6 +15,15 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.10 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.9 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.9.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.23 // indirect
+	github.com/aws/smithy-go v1.25.1 // indirect
 	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 )
---
+++ menu/internal/storage/s3.go
@@ -0,0 +1,45 @@
+package storage
+
+import (
+	"context"
+	"menu/internal/domain"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// S3ImageStorage implements [domain.ImageStorage] using S3 bucket.
+type S3ImageStorage struct {
+	presignClient *s3.PresignClient
+
+	urlExpiry time.Duration
+	bucket    string
+}
+
+// NewS3ImageStorage creates and allocates new [S3ImageStorage].
+func NewS3ImageStorage(client *s3.Client, urlExpiryTine time.Duration, bucket string) *S3ImageStorage {
+	return &S3ImageStorage{
+		presignClient: s3.NewPresignClient(client),
+		urlExpiry:     urlExpiryTine,
+		bucket:        bucket,
+	}
+}
+
+var _ domain.ImageStorage = (*S3ImageStorage)(nil)
+
+func (s *S3ImageStorage) CreateUploadUrl(ctx context.Context, imageKey, contentType string) (string, error) {
+	req, err := s.presignClient.PresignPutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(s.bucket),
+		Key:         aws.String(imageKey),
+		ContentType: aws.String(contentType),
+	}, func(options *s3.PresignOptions) {
+		options.Expires = s.urlExpiry
+	})
+
+	if err != nil {
+		return "", domain.NewError("error presigning url for image uplaod", domain.ErrorCodeInternal, err)
+	}
+
+	return req.URL, nil
+}
