ab8bb214145fdec184ce7519edd44688b739dff7
simone <simonesamardzhiev@gmail.com>
2026-05-18 21:20:00 +0300

    test(menu/storage): Add tests for `Delete` method of `S3ImageStorage`

--- menu/internal/storage/s3_test.go
+++ menu/internal/storage/s3_test.go
@@ -2,13 +2,17 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"menu/internal/domain"
 	"net/http"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/transfermanager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/google/uuid"
 )
 import "github.com/aws/aws-sdk-go-v2/config"
@@ -48,4 +52,46 @@ func TestS3ImageStorageCreateUploadUrl(t *testing.T) {
 	if res.StatusCode != http.StatusOK {
 		t.Fatalf("Unexpected status code: %d", res.StatusCode)
 	}
+}
+
+func TestS3ImageStorageDelete(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping integration test in short mode")
+	}
+	awsConfig, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		t.Fatalf("Error loading aws config: %v", err)
+	}
+
+	client := s3.NewFromConfig(awsConfig, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+	storage := NewS3ImageStorage(client, 15*time.Second, "images")
+
+	key := uuid.NewString()
+	manager := transfermanager.New(client)
+	if _, err := manager.UploadObject(context.Background(), &transfermanager.UploadObjectInput{
+		Bucket: aws.String("images"),
+		Key:    aws.String(key),
+		Body:   strings.NewReader("fakeImage"),
+	}); err != nil {
+		t.Fatalf("Error uploading image: %v", err)
+	}
+
+	if err := storage.Delete(context.Background(), key); err != nil {
+		t.Fatalf("Error deleting image: %v", err)
+	}
+
+	_, err = client.HeadObject(context.Background(), &s3.HeadObjectInput{
+		Bucket: aws.String("images"),
+		Key:    aws.String(key),
+	})
+	if err == nil {
+		t.Fatalf("Want not found error, got nil")
+	}
+
+	if _, ok := errors.AsType[*types.NotFound](err); !ok {
+		t.Fatalf("Want *types.NotFound error, got: %T", err)
+	}
+
 }
