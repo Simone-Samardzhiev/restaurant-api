e597b309d47dcf4437739689d5905c49cf1b0652
simone <simonesamardzhiev@gmail.com>
2026-06-01 01:16:10 +0300

    refactor(menu/rest): Increase cache for product categories

--- menu/internal/rest/category.go
+++ menu/internal/rest/category.go
@@ -102,9 +102,7 @@ func (c *CategoryHandler) GetCategories(ctx *echo.Context) error {
 		})
 	}
 
-	ctx.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=60")
-	ctx.Response().Header().Set("Surrogate-Control", "public, max-age=3600")
-
+	ctx.Response().Header().Set(echo.HeaderCacheControl, "public, max-age=3600, s-maxage=3600")
 	return ctx.JSON(http.StatusOK, response)
 }
 
