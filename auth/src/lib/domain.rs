28c6b97339c52a048da95eb1a84b671d0f13ba5b
simone <simonesamardzhiev@gmail.com>
2026-06-27 15:46:53 +0300

    feat(auth): Add `PasswordHasher`

--- auth/src/lib/domain.rs
+++ auth/src/lib/domain.rs
@@ -1,3 +1,13 @@
+use crate::domain::error::Error;
+
 pub mod error;
 pub mod repositories;
 pub mod user;
+
+
+/// PasswordHasher describes methods for hashing and varifying passwords.
+pub trait PasswordHasher {
+    fn hash(&self, password: &str) -> Result<String, Error>;
+
+    fn verify(&self, password: &str, hash: &str) -> Result<bool, Error>;
+}
---
+++ auth/src/lib/hashers/argon.rs
@@ -0,0 +1,57 @@
+use crate::domain::{PasswordHasher, error::Error};
+use anyhow::Context;
+use argon2::{
+    Argon2, PasswordHash, PasswordHasher as Argon2PasswordHasher, PasswordVerifier,
+    password_hash::{SaltString, rand_core::OsRng},
+};
+
+/// Implementation of [PasswordHasher] using argon.
+pub struct ArgonPasswordHasher<'a> {
+    argon2: Argon2<'a>,
+}
+impl<'a> ArgonPasswordHasher<'a> {
+    pub fn new(argon2: Argon2<'a>) -> Self {
+        Self { argon2 }
+    }
+}
+
+impl<'a> PasswordHasher for ArgonPasswordHasher<'a> {
+    fn hash(&self, password: &str) -> Result<String, Error> {
+        let salt = SaltString::generate(&mut OsRng);
+
+        Ok(self
+            .argon2
+            .hash_password(password.as_bytes(), &salt)
+            .context("Error hashing password")?
+            .to_string())
+    }
+    fn verify(&self, password: &str, hash: &str) -> Result<bool, Error> {
+        let parsed = PasswordHash::new(&hash);
+        match parsed {
+            Ok(hash) => Ok(self
+                .argon2
+                .verify_password(password.as_ref(), &hash)
+                .is_ok()),
+            Err(_) => Ok(false),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_password_hash() {
+        let hasher = ArgonPasswordHasher::new(Argon2::default());
+        let password = "hunter2";
+        hasher.hash(password).unwrap();
+    }
+
+    #[test]
+    fn test_password_verify() {
+        let hasher = ArgonPasswordHasher::new(Argon2::default());
+        let password = "hunter2";
+        let hash = hasher.hash(password).unwrap();
+        assert!(hasher.verify(password, &hash).unwrap());
+    }
+}
